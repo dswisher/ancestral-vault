b08b733d3b1f04a247a47dd8b92754fbe6b7d101
Doug Swisher <big.swish@gmail.com>
2026-01-17 12:36:34 -0600

    Add unit test project, with on token test

---
+++ test/AncestralVault.UnitTests/Common/Database/AncestralVaultDbContextTests.cs
@@ -0,0 +1,36 @@
+using AncestralVault.Common.Database;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AncestralVault.UnitTests.Common.Database
+{
+    public class AncestralVaultDbContextTests
+    {
+        [Fact]
+        public void CanCreateInMemoryDatabase()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<AncestralVaultDbContext>()
+                .UseSqlite("DataSource=:memory:")
+                .Options;
+
+            // Act
+            using var context = new AncestralVaultDbContext(options);
+            context.Database.OpenConnection();
+            context.Database.EnsureCreated();
+
+            // Assert
+            context.Database.CanConnect().Should().BeTrue();
+            context.Personas.Should().NotBeNull();
+            context.Places.Should().NotBeNull();
+            context.DataFiles.Should().NotBeNull();
+            context.Characteristics.Should().NotBeNull();
+            context.JointEvents.Should().NotBeNull();
+            context.SoloEvents.Should().NotBeNull();
+            context.PlaceTypes.Should().NotBeNull();
+            context.Representations.Should().NotBeNull();
+            context.RepresentationTypes.Should().NotBeNull();
+        }
+    }
+}
